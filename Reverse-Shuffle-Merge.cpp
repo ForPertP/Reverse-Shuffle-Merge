214358b1c9e36ff3757db7dee817ba17aad0b654
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-08-06 08:48:57 +0900

    Create Reverse-Shuffle-Merge.cpp

---
+++ Reverse-Shuffle-Merge.cpp
@@ -0,0 +1,40 @@
+string reverseShuffleMerge(string s)
+{
+    int n = s.size();
+
+    vector<char> sarr(s.rbegin(), s.rend());
+    int alpha_size = 26;
+    vector<int> freq(alpha_size, 0);
+
+    for (int i = 0; i < n; ++i)
+    {
+        freq[sarr[i] - 'a']++;
+    }
+
+    vector<int> did_use(alpha_size, 0);
+    vector<int> can_use(freq.begin(), freq.end());
+    vector<char> A;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (did_use[sarr[i] - 'a'] < freq[sarr[i] - 'a'] / 2)
+        {
+            while (A.size() > 0 && sarr[i] < A.back() &&
+                did_use[A.back() - 'a'] + can_use[A.back() - 'a'] - 1 >= freq[A.back() - 'a'] / 2)
+            {
+                did_use[A.back() - 'a']--;
+                A.pop_back();
+            }
+
+            A.push_back(sarr[i]);
+            did_use[sarr[i] - 'a']++;
+            can_use[sarr[i] - 'a']--;
+        }
+        else
+        {
+            can_use[sarr[i] - 'a']--;
+        }
+    }
+
+    return string(A.begin(), A.end());
+}
